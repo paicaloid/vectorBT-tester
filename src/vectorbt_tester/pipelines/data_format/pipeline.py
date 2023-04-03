5a0e6fa3a224570858afc538d0e30681c07798ab
mook <mookfan@gmail.com>
2023-04-03 18:29:59 +0700

    Add data catalog

--- src/vectorbt_tester/pipelines/data_format/pipeline.py
+++ src/vectorbt_tester/pipelines/data_format/pipeline.py
@@ -11,8 +11,62 @@ def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([
         node(
                 func=set_dataframe_index,
-                inputs=['RAW_GOOG', 'params:index_name'],
-                outputs="GOOG",
-                name="set_dataframe_index_node",
+                inputs=['RAW_GOOG_ADX', 'params:index_name'],
+                outputs="GOOG_ADX",
+                name="ADX_dataframe_index_node",
+            ),
+        node(
+                func=set_dataframe_index,
+                inputs=['RAW_GOOG_BB', 'params:index_name'],
+                outputs="GOOG_BB",
+                name="BB_dataframe_index_node",
+            ),
+        node(
+                func=set_dataframe_index,
+                inputs=['RAW_GOOG_EMA_5', 'params:index_name'],
+                outputs="GOOG_EMA_5",
+                name="EMA_5_dataframe_index_node",
+            ),
+        node(
+                func=set_dataframe_index,
+                inputs=['RAW_GOOG_EMA_35', 'params:index_name'],
+                outputs="GOOG_EMA_35",
+                name="EMA_35_dataframe_index_node",
+            ),
+        node(
+                func=set_dataframe_index,
+                inputs=['RAW_GOOG_MACD', 'params:index_name'],
+                outputs="GOOG_MACD",
+                name="MACD_dataframe_index_node",
+            ),
+        node(
+                func=set_dataframe_index,
+                inputs=['RAW_GOOG_OBV', 'params:index_name'],
+                outputs="GOOG_OBV",
+                name="OBV_dataframe_index_node",
+            ),
+        node(
+                func=set_dataframe_index,
+                inputs=['RAW_GOOG_RSI', 'params:index_name'],
+                outputs="GOOG_RSI",
+                name="RSI_dataframe_index_node",
+            ),
+        node(
+                func=set_dataframe_index,
+                inputs=['RAW_GOOG_SAR', 'params:index_name'],
+                outputs="GOOG_SAR",
+                name="SAR_dataframe_index_node",
+            ),
+        node(
+                func=set_dataframe_index,
+                inputs=['RAW_GOOG_STOCH', 'params:index_name'],
+                outputs="GOOG_STOCH",
+                name="STOCH_dataframe_index_node",
+            ),
+        node(
+                func=set_dataframe_index,
+                inputs=['RAW_GOOG_WR', 'params:index_name'],
+                outputs="GOOG_WR",
+                name="WR_dataframe_index_node",
             ),
     ])
