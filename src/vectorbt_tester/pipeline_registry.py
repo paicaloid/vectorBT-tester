05698dbf663a5a652972e4d2e66c1f68e230b2cc
mook <mookfan@gmail.com>
2023-04-10 11:44:00 +0700

    Add example of custom pipeline name on kedro-viz

--- src/vectorbt_tester/pipeline_registry.py
+++ src/vectorbt_tester/pipeline_registry.py
@@ -4,6 +4,9 @@ from typing import Dict
 from kedro.framework.project import find_pipelines
 from kedro.pipeline import Pipeline
 
+from vectorbt_tester.pipelines import data_format as df
+from vectorbt_tester.pipelines import merge_data as md
+
 
 def register_pipelines() -> Dict[str, Pipeline]:
     """Register the project's pipelines.
@@ -11,6 +14,18 @@ def register_pipelines() -> Dict[str, Pipeline]:
     Returns:
         A mapping from pipeline names to ``Pipeline`` objects.
     """
+    # 0. In DEFAULT case
     pipelines = find_pipelines()
     pipelines["__default__"] = sum(pipelines.values())
+
     return pipelines
+    
+    # # 1. In case that you want to rename pipeline
+    # data_format_pipeline = df.create_pipeline()
+    # merge_data_pipeline = md.create_pipeline()
+
+    # return {
+    #     "df": data_format_pipeline,
+    #     "md": merge_data_pipeline,
+    #     "__default__": data_format_pipeline + merge_data_pipeline,
+    # }
